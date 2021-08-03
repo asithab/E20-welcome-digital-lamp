f5b1c463b64bb1289e591271e33aa5c7ba7e974f
shyaman <shayaman321@gmail.com>
2021-08-03 16:29:07 +0530

    update map ui

--- js/maps.js
+++ js/maps.js
@@ -3,7 +3,9 @@ function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
         center: { lat: 7.8731, lng: 80.7718 },
-
+        disableDefaultUI: true,
+        scrollwheel: false,
+        draggable: false,
         mapId: "e73be1757f1fdda8"
     });
 
@@ -19,7 +21,7 @@ function initMap() {
             var name = stud[i].Name;
             var lat = stud[i].Latitude;
             var lon = stud[i].Longitude;
-            setTimeout(placeMarkerAndPanTo, i * 1000, new google.maps.LatLng(lat, lon), name, map)
+            setTimeout(placeMarker, i * 1000, new google.maps.LatLng(lat, lon), map)
 
         }
     })
@@ -48,6 +50,23 @@ function initMap() {
 
 }
 
+
+function placeMarker(latLng, map) {
+    var icon = {
+        url: "img/oil-lamp.png", // url
+        scaledSize: new google.maps.Size(40, 40), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(20, 40) // anchor
+    };
+
+    map.panTo(latLng);
+
+    new google.maps.Marker({
+        position: latLng,
+        map: map,
+        icon: icon,
+    });
+}
 
 function placeMarkerAndPanTo(latLng, name, map) {
     var icon = {
