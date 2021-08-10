b929d9934a6fa767a19a5bcc5537a064ef155b4e
shyaman <shayaman321@gmail.com>
2021-08-10 19:38:16 +0530

    add vc deans dep.heads

--- js/maps.js
+++ js/maps.js
@@ -1,13 +1,20 @@
-let map
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
+    let map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
         center: { lat: 7.8731, lng: 80.7718 },
         disableDefaultUI: true,
-        scrollwheel: false,
         draggable: false,
         mapId: "e73be1757f1fdda8"
     });
+
+    var slbounds = new google.maps.LatLngBounds();
+    slbounds.extend(new google.maps.LatLng(9.953036, 82.268190));
+    slbounds.extend(new google.maps.LatLng(5.790138, 78.785327));
+    map.fitBounds(slbounds);
+
+    var perabounds = new google.maps.LatLngBounds();
+    perabounds.extend(new google.maps.LatLng(7.271635897056729, 80.61025352366207));
+    perabounds.extend(new google.maps.LatLng(7.250887, 80.588064));
 
     var stud;
     $.getJSON("stu.json", function (json) {
@@ -16,8 +23,8 @@ function initMap() {
 
     // jquery on button click
     $("#student").click(function () {
-        map.panTo(new google.maps.LatLng('7.8731', '80.7718'))
-        map.setZoom(8);
+        $(this).prop('disabled', true);
+        map.fitBounds(slbounds);
         // iterate over stud 
         for (var i = 0; i < stud.length; i++) {
             // var name = stud[i].Name;
@@ -28,12 +35,48 @@ function initMap() {
         }
     })
 
+    $("#vc").click(function () {
+        $(this).prop('disabled', true);
+        map.fitBounds(perabounds);
+        placeMarker(new google.maps.LatLng(7.254017744015974, 80.59674509791466), map);
+    })
+
+    $("#deans").click(function () {
+        $(this).prop('disabled', true);
+        map.fitBounds(perabounds);
+        placeMarker(new google.maps.LatLng(7.262360, 80.598332), map);
+        placeMarker(new google.maps.LatLng(7.2640113257763295, 80.59897901185354), map);
+        placeMarker(new google.maps.LatLng(7.259764416540214, 80.5991103385456), map);
+        placeMarker(new google.maps.LatLng(7.253653010403313, 80.59208965397332), map);
+        placeMarker(new google.maps.LatLng(7.266467160619582, 80.5993644105187), map);
+        placeMarker(new google.maps.LatLng(7.254932480063095, 80.59733625362125), map);
+        placeMarker(new google.maps.LatLng(7.260117551945774, 80.59535707875777), map);
+        placeMarker(new google.maps.LatLng(7.268390657877636, 80.60673782293601), map);
+        placeMarker(new google.maps.LatLng(7.256974294284219, 80.59561192848471), map);
+    })
+
+    $("#dh").click(function () {
+        $(this).prop('disabled', true);
+        map.fitBounds(perabounds);
+        placeMarker(new google.maps.LatLng(7.2643327275129, 80.59680694773525), map);
+        placeMarker(new google.maps.LatLng(7.26388573308274, 80.59561604697777), map);
+        placeMarker(new google.maps.LatLng(7.262480890551459, 80.59553021629253), map);
+        placeMarker(new google.maps.LatLng(7.262097750917425, 80.59676403239264), map);
+        placeMarker(new google.maps.LatLng(7.261139900403967, 80.5976652545875), map);
+        placeMarker(new google.maps.LatLng(7.260980258453375, 80.59475774012553), map);
+        placeMarker(new google.maps.LatLng(7.260778045234578, 80.59688204958482), map);
+        placeMarker(new google.maps.LatLng(7.260437475397385, 80.59650654033697), map);
+    })
+
+
     $("#india").click(function () {
+        $(this).prop('disabled', true);
         map.setZoom(5);
         placeMarkerAndPanTo(new google.maps.LatLng("28.6527881", "77.2113932"), map);
     })
 
     $("#nepal").click(function () {
+        $(this).prop('disabled', true);
         map.setZoom(5);
         placeMarkerAndPanTo(new google.maps.LatLng("27.649505", "85.405770"), map);
     })
